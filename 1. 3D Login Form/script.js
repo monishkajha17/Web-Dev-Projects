d7df3a226cde2014305f0c3f6af7d50cfe5c0c17
Monishka Jha <jhamonishka@gmail.com>
2023-01-23 12:03:16 +0530

    Updated Changes

---
+++ 1. 3D Login Form/script.js
@@ -0,0 +1,12 @@
+const signUpBtn = document.querySelector(".signup-btn");
+const signInBtn = document.querySelector(".signin-btn");
+const formsWrapper=document.querySelector(".forms-wrapper");
+signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formsWrapper.classList.add("change");
+  });
+  
+  signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formsWrapper.classList.remove("change");
+  });
